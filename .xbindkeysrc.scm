059615d6ff92c1d2794dfd785c883b11ad0ab92b
Michael Jean <mikulely@gmail.com>
2013-05-29 21:52:28 +0800

    Add multimedia hotkey

---
+++ .xbindkeysrc.scm
@@ -0,0 +1,3 @@
+(xbindkey '("XF86AudioMute") "amixer -q sset Master toggle")
+(xbindkey '("XF86AudioRaiseVolume") "amixer -q sset Master 1%+")
+(xbindkey '("XF86AudioLowerVolume") "amixer -q sset Master 1%-")
